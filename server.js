cbb63b6958ea23332dcac937b5bf764600ca1e4e
root <root@cv5569903.novalocal>
2026-03-19 12:32:53 +0300

    set server port 3001

--- server.js
+++ server.js
@@ -1,1 +1,6 @@
-console.log("deploy check 1773912190"); setInterval(() => {}, 1000);
+const http = require("http");
+const port = process.env.PORT || 3001;
+http.createServer((req, res) => {
+  res.writeHead(200, { "Content-Type": "text/plain; charset=utf-8" });
+  res.end("kz works on port " + port);
+}).listen(port, () => console.log("kz server started on " + port));
